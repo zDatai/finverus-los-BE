06d83faf3d7fdc0c47a9afa0ea2407f03746d0c5
Udara Wikum <udara@zdatai.com>
2025-03-25 22:09:44 +0530

    Chat management entities added

---
+++ src/main/java/com/zdatai/finverus/model/application/OptionChoices.java
@@ -0,0 +1,30 @@
+package com.zdatai.finverus.model.application;
+
+import com.zdatai.finverus.enums.SourceTypeEnum;
+import com.zdatai.finverus.model.AuditCreateUser;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Table(name = "option_choices")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
+@EqualsAndHashCode(callSuper = false)
+public class OptionChoices extends AuditCreateUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "option_source_id", nullable = false, unique = true)
+    private Long optionSourceId;
+
+    @Column(name = "source_type", nullable = false, columnDefinition = "VARCHAR(255) DEFAULT STATIC")
+    @Enumerated(EnumType.STRING)
+    private SourceTypeEnum sourceType;
+
+    @Column(name = "description")
+    private String description;
+
+}
