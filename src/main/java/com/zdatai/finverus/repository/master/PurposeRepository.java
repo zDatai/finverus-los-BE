64d90cab2c1953a1095aa619ca8bb8553f8f5edb
uresh pinidiya <99754729+IT21158636@users.noreply.github.com>
2025-03-28 01:05:50 +0530

    fix purpose category issues

--- src/main/java/com/zdatai/finverus/repository/master/PurposeRepository.java
+++ src/main/java/com/zdatai/finverus/repository/master/PurposeRepository.java
@@ -1,8 +1,13 @@
 package com.zdatai.finverus.repository.master;
 
+import com.zdatai.finverus.model.AuditModifyUser;
 import com.zdatai.finverus.model.master.Purpose;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PurposeRepository extends JpaRepository<Purpose, Long> {}
+public interface PurposeRepository extends JpaRepository<Purpose, Long> {
+    Page<Purpose> findByStatus(AuditModifyUser.Status status, Pageable pageable);
+}
